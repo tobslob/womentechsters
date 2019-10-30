58cf67b95a6e8c41b4b61ad630a5671e7eada947
tobslob <kazmobileapp@gmail.com>
2019-10-30 16:05:58 +0100

    Introduction to javascript

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// es6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+// es5
+var carbonara = "You can make carbonara with" + pasta + "," + meat + "," + "and a sauce made with" + sauce + ".";
+
+
